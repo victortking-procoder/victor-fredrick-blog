b9c5c7dddad6ac580df62fd92c6fb8453eac0ce9
Victor Fredrick <fredrickvictor01@outlook.com>
2025-08-31 18:42:26 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -54,12 +54,14 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
- 'DEFAULT_AUTHENTICATION_CLASSES': [
-     'rest_framework.authentication.TokenAuthentication',
- ],
- 'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
- ]   
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
 }
 
 
